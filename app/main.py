758ce33523b56d74bb76d939a404f0175e5e8b39
elkindman <elkanahkindness@gmail.com>
2024-09-15 20:37:20 +0100

    implemented authentication and signup

--- app/main.py
+++ app/main.py
@@ -1,14 +1,52 @@
 '''main program logic
 '''
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash, request
+from models import Admin, Post
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'elkindman2002p.'
 
-#database setup
-db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+# ---> home/landing page
 @app.route('/')
-def landing():
+def home():
     return render_template('home.html')
 
+#signup
+@app.route('/signup', methods=['GET', 'POST'])
+def siginup():
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+#Datacheck ---->
+        admin_exists = Admin.query.filter((Admin.name == name) | (Admin.email == email)).first()
+        if admin_exists:
+            flash('Email or Media Admin already exists!', 'danger')
+            return redirect(url_for('signup'))
+
+#Create new admin
+        new_admin = Admin(name=name, username=username, email=email)
+        new_admin.set_password(password)
+#Adding new_admin to database
+        try:
+            db.session.add(new_admin)
+            db.session.commit()
+            flash('You have successfully become a Media Admin! Please login', 'success')
+            return redirect(url_for('login'))
+        except Exception as e:
+            db.session.rollback()
+            flash('Error creating account. Try again.', 'danger')
+
+    return render_template('signup.html')
+
+
+#database instance
+db = SQLAlchemy(app)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,28 @@
 '''
 Database models used for the program
 '''
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from main import db, app
 
-class Admin(db.Model):
+class Admin(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=True, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    posts = db.relationships('Post', backref='author', lazy=True)
+    Hashed_password = db.Column(db.String(30), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), unique=True, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    admin_id = db.Column(db.Integer, db.ForeignKey('Admin.id'), nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
+
+    def set_password(self, password):
+        self.Hashed_password = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.Hashed_password, password)
